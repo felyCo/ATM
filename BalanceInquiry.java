114abdf26c01e4777f8f615ab263d3a8894446ae
FelyCo <32365871+felyCo@users.noreply.github.com>
2019-08-21 13:04:41 +0300

    Update BalanceInquiry.java

--- BalanceInquiry.java
+++ BalanceInquiry.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package atm;
 
 /**
  *
- * @author Administrator
+ * @author Felix
  */
 // BalanceInquiry.java
 // Represents a balance inquiry ATM transaction
