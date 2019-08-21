77ef5839108a221f3cc750b8fcb1bf86915b1762
FelyCo <32365871+felyCo@users.noreply.github.com>
2019-08-21 13:05:17 +0300

    Update Withdrawal.java

--- Withdrawal.java
+++ Withdrawal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package atm;
 
 /**
@@ -30,7 +26,7 @@ package atm;
  *      Class ATMCaseStudy
  * 
  * 
- * @author Administrator
+ * @author Felix
  */
 
 //Class Withdrawal represents an ATM withdrawal transaction
