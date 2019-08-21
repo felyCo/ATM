5c529bc744bf54b1952f7cc06505aa1ac14457ed
FelyCo <32365871+felyCo@users.noreply.github.com>
2019-08-21 13:06:38 +0300

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package atm;
 
 /**
  *
- * @author Administrator
+ * @author Felix
  */
 // Transaction.java
 // Abstract superclass Transaction represents an ATM transaction
